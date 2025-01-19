a17c24d635235ec2eb824154b269e5ea22f33718
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-01-19 07:55:59 +0000

    a basic web server up and running

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "time"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+    currentTime := time.Now().Format("15:04:05")
+    fmt.Fprintf(w, "Current Server Time: %s", currentTime)
+}
 
 func main() {
+    http.HandleFunc("/", helloHandler)
+    http.HandleFunc("/time", timeHandler)
 
-	fmt.Println("Hello, World!")
+    fmt.Println("Starting server on :8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        fmt.Println("Server failed:", err)
+    }
 }
