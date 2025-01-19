027dd407fdb80bb92b03036845bb8a26c99a8831
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-01-19 08:09:45 +0000

    adding static files and mai.go now serves those files

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// main.go
 package main
 
 import (
@@ -16,7 +17,17 @@ func timeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/", helloHandler)
+    // Serve static files from the "static" directory
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    // Handle the root path to serve the index.html
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "static/index.html")
+    })
+
+    // Existing handlers
+    http.HandleFunc("/hello", helloHandler)
     http.HandleFunc("/time", timeHandler)
 
     fmt.Println("Starting server on :8080")
